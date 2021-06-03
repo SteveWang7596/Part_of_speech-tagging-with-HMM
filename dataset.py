616617dc832978f880100cb24ba7435fdf94abed
Scott Hallauer <scott.hallauer@gmail.com>
2021-06-03 13:32:05 +0200

    Add script to read and divide datasets

---
+++ dataset.py
@@ -0,0 +1,45 @@
+# dataset.py
+# Scott Hallauer
+# 3 June 2021
+
+# Script to read in CSV datasets. Includes functionality to divide dataset into 
+# training and development sets.
+
+import csv
+import math
+
+train_filename = "data/raw/GOV-ZA.50000TrainingSet.af.pos.full.csv"
+test_filename = "data/raw/GOV-ZA.5000TestSet.af.pos.full.csv"
+
+def load(name = "train"):
+
+  if name == "train":
+    selected_filename = train_filename
+  else:
+    selected_filename = test_filename
+
+  with open(selected_filename) as input_file:
+    input_reader = csv.reader(input_file, delimiter=',')
+    dataset = []
+    sentence_words = []
+    sentence_tags = []
+    line_count = 0
+    for row in input_reader:
+      word = row[0]
+      tag = row[1]
+      if word == "" and tag == "":
+        dataset.append({"words": sentence_words, "tags": sentence_tags})
+        sentence_words = []
+        sentence_tags = []
+        continue
+      if line_count > 0:
+        sentence_words.append(word)
+        sentence_tags.append(tag)
+      line_count += 1
+    return dataset
+
+
+def subset(dataset, start_proportion = 0.0, end_proportion = 1.0):
+  start_idx = math.floor(len(dataset) * start_proportion)
+  end_idx = math.floor(len(dataset) * end_proportion)
+  return dataset[start_idx:end_idx]
