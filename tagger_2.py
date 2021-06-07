617c21c2ff7ff115453756fe06011c9ba1c82baf
SteveWang7596 <37341748+SteveWang7596@users.noreply.github.com>
2021-06-07 18:29:43 +0200

    Update tagger_2.py with <unk> tags

--- tagger_2.py
+++ tagger_2.py
@@ -12,12 +12,12 @@ import statistics
 ###########################################################
 
 train_set = dataset.load("train")
+train_set = dataset.prepare(train_set)
 test_set = dataset.load("test")
 q_dist = statistics.get_transition_distribution(train_set, smoothing="laplace")
 e_dist = statistics.get_emission_distribution(train_set, smoothing="laplace")
 trained_POS_tags = statistics.get_tags(train_set)
 trained_words = statistics.get_words(train_set)
-pos_tags = statistics.get_tags(test_set)
 trigram_dist = statistics.get_trigram_distribution(train_set)
 
 #############################################
@@ -33,29 +33,21 @@ def populate_lattice(lattice):
     for i in range(len(lattice)):
         word = lattice[i][0]
         if word not in trained_words:
-            find_word = "UNKNOWN_WORD"
+            find_word = "<UNK>"
         else:
             find_word = word
         # For each word and each POS tag
         for pos_tag in lattice[i][1]:
-            if pos_tag not in trained_POS_tags:
-                find_pos_tag = "UNKNOWN"
-            else:
-                find_pos_tag = pos_tag
             if i == 0:
                 # If it is the first word then pi is the probability of the tag on condition of the word
-                lattice[i][1][pos_tag][0] = float(q_dist["START"][find_pos_tag]) * float(e_dist[find_word][find_pos_tag])
+                lattice[i][1][pos_tag][0] = float(q_dist["START"][pos_tag]) * float(e_dist[find_word][pos_tag])
             elif i == 1:
                 # If i is at the 2nd word then pi is the probability of the tag on condition of the previous word and "START"
                 pi = None
                 back_pointer = None
                 temp = None
                 for prev_values in lattice[i-1][1]:
-                    if prev_values not in trained_POS_tags:
-                        find_prev_values = "UNKNOWN"
-                    else:
-                        find_prev_values = prev_values
-                    temp = float(lattice[i-1][1][prev_values][0]) * float(trigram_dist["START"][find_prev_values][find_pos_tag]) * float(e_dist[find_word][find_pos_tag])
+                    temp = float(lattice[i-1][1][prev_values][0]) * float(trigram_dist["START"][prev_values][pos_tag]) * float(e_dist[find_word][pos_tag])
                     if pi == None or temp > pi:
                         pi = temp
                         back_pointer = prev_values
@@ -66,16 +58,8 @@ def populate_lattice(lattice):
                 back_pointer = None
                 temp = None
                 for prev_values in lattice[i - 1][1]:
-                    if prev_values not in trained_POS_tags:
-                        find_prev_values = "UNKNOWN"
-                    else:
-                        find_prev_values = prev_values
                     for pre_prev_value in lattice[i - 2][1]:
-                        if pre_prev_value not in trained_POS_tags:
-                            find_pre_prev_value = "UNKNOWN"
-                        else:
-                            find_pre_prev_value = pre_prev_value
-                        temp = float(lattice[i-1][1][prev_values][0]) * float(trigram_dist[find_pre_prev_value][find_prev_values][find_pos_tag]) * float(e_dist[find_word][find_pos_tag])
+                        temp = float(lattice[i-1][1][prev_values][0]) * float(trigram_dist[pre_prev_value][prev_values][pos_tag]) * float(e_dist[find_word][pos_tag])
                         if pi == None or temp > pi:
                             pi = temp
                             back_pointer = prev_values
@@ -109,7 +93,7 @@ for test_sentence_set in test_set:
     test_sentence_tags = test_sentence_set.get('tags')
     lattice = []
     row = {}
-    for POS_tag in statistics.get_tags(test_set):
+    for POS_tag in trained_POS_tags:
         row[POS_tag] = [0, None]
     for word in test_sentence_words:
         lattice.append([word, copy.deepcopy(row)])
