ed4b3250ab7d651bf235d1b5c4d386dbd9619f47
SteveWang7596 <37341748+SteveWang7596@users.noreply.github.com>
2021-06-07 15:51:36 +0200

    Updated HHM tagger to use the <unk> tag

--- HHM_tagger.py
+++ HHM_tagger.py
@@ -12,12 +12,12 @@ import statistics
 ###########################################################
 
 train_set = dataset.load("train")
+train_set = dataset.prepare(train_set)
 test_set = dataset.load("test")
 q_dist = statistics.get_transition_distribution(train_set, smoothing="laplace")
 e_dist = statistics.get_emission_distribution(train_set, smoothing="laplace")
 trained_POS_tags = statistics.get_tags(train_set)
 trained_words = statistics.get_words(train_set)
-POS_tags = statistics.get_tags(test_set)
 
 #############################################
 # word_1, {POS_1:[x,y], POS_2:[x,y], ...}   #
@@ -32,36 +32,24 @@ def populate_lattice(lattice):
     # For each row in the lattice
     for i in range(len(lattice)):
         word = lattice[i][0]
+        if word not in trained_words:
+            find_word = "<UNK>"
+        else:
+            find_word = word
         # For each word and each POS tag
         for pos_tag in lattice[i][1]:
-            if pos_tag not in trained_POS_tags:
-                find_pos_tag = "UNKNOWN"
-            else:
-                find_pos_tag = pos_tag
-            if word not in trained_words:
-                find_word = "UNKNOWN_WORD"
-            else:
-                find_word = word
             if i == 0:
                 # If it is the first word then pi is the probability of the tag on condition of the word
-                lattice[i][1][pos_tag][0] = float(q_dist["START"][find_pos_tag]) * float(e_dist[find_word][find_pos_tag])
+                lattice[i][1][pos_tag][0] = float(q_dist["START"][pos_tag]) * float(e_dist[find_word][pos_tag])
             else:
                 pi = None
                 back_pointer = None
                 temp = None
                 for prev_values in lattice[i - 1][1]:
-                    if prev_values not in trained_POS_tags:
-                        find_prev_values = "UNKNOWN"
-                    else:
-                        find_prev_values = prev_values
-                    if word not in trained_words:
-                        find_word = "UNKNOWN_WORD"
-                    else:
-                        find_word = word
                     # Let temp value be pi of previous values * probability of current tag given previous tag *
                     # probablity of tag given word
                     #print(word)
-                    temp = float(lattice[i - 1][1][prev_values][0]) * float(q_dist[find_prev_values][find_pos_tag]) * float(e_dist[find_word][find_pos_tag])
+                    temp = float(lattice[i - 1][1][prev_values][0]) * float(q_dist[prev_values][pos_tag]) * float(e_dist[find_word][pos_tag])
                     if pi == None or temp > pi:
                         pi = temp
                         back_pointer = prev_values
@@ -94,7 +82,7 @@ for test_sentence_set in test_set:
     test_sentence_tags = test_sentence_set.get('tags')
     lattice = []
     row = {}
-    for POS_tag in POS_tags:
+    for POS_tag in trained_POS_tags:
         row[POS_tag] = [0, None]
     for word in test_sentence_words:
         lattice.append([word, copy.deepcopy(row)])
--- tester.py
+++ tester.py
@@ -1,3 +1,5 @@
+import csv
+
 import dataset
 import statistics
 
@@ -5,13 +7,36 @@ import statistics
 # PRE-PROCESSING                                          #
 ###########################################################
 
+test_filename = "data/raw/tester.csv"
+
 train_set = dataset.load("train")
 test_set = dataset.load()
 q_dist = statistics.get_transition_distribution(train_set, smoothing="laplace")
 e_dist = statistics.get_emission_distribution(train_set, smoothing="laplace")
 
-tags = statistics.get_tags(train_set)
+def load():
+  """Return the requested dataset as a list of word-tag paired sentences."""
+  with open(test_filename) as input_file:
+    input_reader = csv.reader(input_file, delimiter=',')
+    dataset = []
+    sentence_words = []
+    sentence_tags = []
+    line_count = 0
+    for row in input_reader:
+      word = row[0]
+      tag = row[1]
+      if word == "" and tag == "":
+        dataset.append({"words": sentence_words, "tags": sentence_tags})
+        sentence_words = []
+        sentence_tags = []
+        continue
+      if line_count > 0:
+        sentence_words.append(word)
+        sentence_tags.append(tag)
+      line_count += 1
+    return dataset
 
-for tag_1 in tags:
-    for tag_2 in tags:
-        print (q_dist[tag_1][tag_2])
+data = load()
+#tri_gram_counts = statistics.get_trigram_counts(data)
+tri_gram_distrbution = statistics.get_trigram_distribution(data)
+print(tri_gram_distrbution)
